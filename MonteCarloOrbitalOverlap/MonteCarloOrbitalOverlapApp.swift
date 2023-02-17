32b56fecf01c63ba23520650c584addb24fc8872
Andre Guardia <andre.guardiaq@gmail.com>
2023-02-17 14:11:58 -0600

    Initial Commit

---
+++ MonteCarloOrbitalOverlap/MonteCarloOrbitalOverlapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonteCarloOrbitalOverlapApp.swift
+//  MonteCarloOrbitalOverlap
+//
+//  Created by IIT Phys 440 on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MonteCarloOrbitalOverlapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
